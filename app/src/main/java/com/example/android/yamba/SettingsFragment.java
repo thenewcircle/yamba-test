0d9f07f10c864232f2c9f9f7120e8e3ef86630f9
Dave Smith <smith@wiresareobsolete.com>
2015-03-05 15:19:01 -0700

    Remove periodic notification steps.

--- app/src/main/java/com/example/android/yamba/SettingsFragment.java
+++ app/src/main/java/com/example/android/yamba/SettingsFragment.java
@@ -1,15 +1,9 @@
 package com.example.android.yamba;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
 
-public class SettingsFragment extends PreferenceFragment implements
-		OnSharedPreferenceChangeListener {
-	private SharedPreferences mPreferences;
+public class SettingsFragment extends PreferenceFragment {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -17,22 +11,4 @@ public class SettingsFragment extends PreferenceFragment implements
 		addPreferencesFromResource(R.xml.settings);
 	}
 
-	@Override
-	public void onStart() {
-		super.onStart();
-		mPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-		mPreferences.registerOnSharedPreferenceChangeListener(this);
-	}
-
-	@Override
-	public void onStop() {
-		super.onStop();
-		mPreferences.unregisterOnSharedPreferenceChangeListener(this);
-	}
-
-	@Override
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-			String key) {
-		getActivity().sendBroadcast( new Intent("com.example.android.yamba.action.UPDATED_INTERVAL") );
-	}
 }
