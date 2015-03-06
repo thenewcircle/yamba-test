329098f848faaa13b67eb64a0d7ff68f36468d1f
Dave Smith <smith@wiresareobsolete.com>
2015-03-06 16:09:09 -0700

    Remove activity customizations.

--- app/src/main/java/com/example/android/yamba/StatusActivity.java
+++ app/src/main/java/com/example/android/yamba/StatusActivity.java
@@ -1,71 +1,16 @@
 package com.example.android.yamba;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-public class StatusActivity extends ActionBarActivity implements
-        View.OnClickListener, TextWatcher {
+public class StatusActivity extends ActionBarActivity {
     private static final String TAG = StatusActivity.class.getSimpleName();
-
-    private Button mButtonTweet;
-    private EditText mTextStatus;
-    private TextView mTextCount;
-    private int mDefaultColor;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_status);
         Log.d(TAG, "onCreated");
-
-        mButtonTweet = (Button) findViewById(R.id.status_button_tweet);
-        mTextStatus = (EditText) findViewById(R.id.status_text);
-        mTextCount = (TextView) findViewById(R.id.status_text_count);
-
-        mDefaultColor = mTextCount.getTextColors().getDefaultColor();
-
-        mButtonTweet.setOnClickListener(this);
-        mTextStatus.addTextChangedListener(this);
-
-        // This also triggers our text watcher to set the initial character count
-        mTextStatus.setText(null);
-    }
-
-    // -- TextWatcher --
-
-    @Override
-    public void afterTextChanged(Editable s) {
-        int count = 140 - s.length();
-        mTextCount.setText(Integer.toString(count));
-
-        if (count < 10) {
-            mTextCount.setTextColor(Color.RED);
-        } else {
-            mTextCount.setTextColor(mDefaultColor);
-        }
-
-        mButtonTweet.setEnabled(count >= 0);
-    }
-
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) { }
-
-    // -- OnClickListener --
-
-    @Override
-    public void onClick(View v) {
-        Log.d(TAG, "onClicked");
     }
 
 }
