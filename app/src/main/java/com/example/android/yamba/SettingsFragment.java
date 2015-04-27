49ccb78aca3f6249154e1e0a08283378e857b0a8
Dave Smith <smith@wiresareobsolete.com>
2015-04-27 15:37:24 -0600

    Periodic lab solution.

--- app/src/main/java/com/example/android/yamba/SettingsFragment.java
+++ app/src/main/java/com/example/android/yamba/SettingsFragment.java
@@ -1,9 +1,15 @@
 package com.example.android.yamba;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private SharedPreferences mPreferences;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -11,4 +17,24 @@ public class SettingsFragment extends PreferenceFragment {
         addPreferencesFromResource(R.xml.settings);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mPreferences =
+                PreferenceManager.getDefaultSharedPreferences(getActivity());
+        mPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mPreferences.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                          String key) {
+        getActivity().sendBroadcast(new Intent(
+                "com.example.android.yamba.action.UPDATED_INTERVAL"));
+    }
 }
