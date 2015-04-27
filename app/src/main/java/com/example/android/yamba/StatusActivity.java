971c344cdc819826b0ead6c2f8ca0998f51a8600
Dave Smith <smith@wiresareobsolete.com>
2015-04-27 12:37:53 -0600

    Status activity lab solution.

---
+++ app/src/main/java/com/example/android/yamba/StatusActivity.java
@@ -0,0 +1,92 @@
+package com.example.android.yamba;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class StatusActivity extends AppCompatActivity implements
+        View.OnClickListener, TextWatcher {
+    private int mDefaultColor;
+
+    private Button mPostButton;
+    private EditText mTextStatus;
+    private TextView mTextCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+
+        mPostButton = (Button) findViewById(R.id.status_button);
+        mTextStatus = (EditText) findViewById(R.id.status_text);
+        mTextCount = (TextView) findViewById(R.id.status_text_count);
+
+        mPostButton.setOnClickListener(this);
+        mTextStatus.addTextChangedListener(this);
+
+        mDefaultColor = mTextCount.getTextColors().getDefaultColor();
+
+        mTextStatus.setText(null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_yamba, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "Click!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        int count = 140 - s.length();
+        mTextCount.setText(Integer.toString(count));
+
+        if (count < 10) {
+            mTextCount.setTextColor(Color.RED);
+        } else {
+            mTextCount.setTextColor(mDefaultColor);
+        }
+
+        mPostButton.setEnabled(count >= 0);
+    }
+}
